c96f3c43b5f991c1399317779f87a9aa303deba3
DESKTOP-2M8HV2R\user <dwiyanyogaswara@gmail.com>
2023-06-20 09:21:54 +0700

    Step 3: create app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tes_firebase/screens/auth/auth.dart';
+import 'package:tes_firebase/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Auth();
+  }
+}
